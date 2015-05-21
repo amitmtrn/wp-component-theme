6e98dfd55c3768822b64fc6edb12cddb44824f9a
Amit Choukroun <amit@mtrn.co.il>
2015-05-21 08:36:42 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,13 +2,13 @@
 
 require_once dirname(__FILE__) . '/api.php';
 
-function sponzrit_scripts() {
+function component_theme_scripts() {
     wp_enqueue_style('main', get_stylesheet_uri());
     wp_enqueue_script('riotjs', get_template_directory_uri() . '/resources/riot.min.js', array(), '2.0.15', true);
     wp_enqueue_script('app', get_template_directory_uri() . '/app.js', array('riotjs', 'jquery'), '15.5.16', true);
 }
 
-add_action('wp_enqueue_scripts', 'sponzrit_scripts');
+add_action('wp_enqueue_scripts', 'component_theme_scripts');
 
 function add_ajaxurl() {
     include_once dirname(__FILE__) . '/ajaxurl.php';
